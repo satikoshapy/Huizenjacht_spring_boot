ab1159c956497f6b05ced6cd8b0d3fac72f8551a
satikoshapy <satikoshapy@yahoo.com>
2024-10-09 16:42:21 +0200

    update with hashmap collection

--- src/main/java/be/pxl/huizenjacht/HouseService.java
+++ src/main/java/be/pxl/huizenjacht/HouseService.java
@@ -3,14 +3,22 @@ package be.pxl.huizenjacht;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class HouseService {
     private final List<House> houses = new ArrayList<>();
 
+    Map<String, House> hashedHouses = new HashMap<>();
+
+
+
     public void addHouse(House house ){
+
         houses.add(house);
+        hashedHouses.put(house.getCode(), house);
     }
 
     public List<House> getHouses(){
@@ -18,7 +26,7 @@ public class HouseService {
     }
 
     public void updateHouseByCode(String code, House house){
-        int index = -1;
+        /*int index = -1;
         for (int i = 0; i < houses.size(); i++) {
             if (houses.get(i).getCode().equals(code)) {
                 index = i;
@@ -27,13 +35,20 @@ public class HouseService {
         }
         if (index != -1){
             houses.set(index, house);
+        }*/
+        House houseByCode = hashedHouses.get(code);
+        if (houseByCode != null){
+            int houseIndex = houses.indexOf(houseByCode);
+            houses.set(houseIndex, house);
         }
+
     }
 
     public void deleteHouse(String code){
         for (House house : houses) {
             if (house.getCode().equals(code)) {
                 houses.remove(house);
+                hashedHouses.remove(code);
             }
         }
     }
